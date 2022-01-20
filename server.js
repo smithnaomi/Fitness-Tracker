a9dbeff0f9643240df8c5379e402083cf6c0c87b
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-20 16:06:56 -0500

    Added const, mongo

---
+++ server.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const express = require("express");
+const logger = require("morgan");
+const routes = require("./routes");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
+
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
